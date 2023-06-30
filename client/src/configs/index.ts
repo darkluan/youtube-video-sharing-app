2acec2a7c2b2053ae5df0fea7e1ca94c33c4b65b
luanpb <luanphamyoyo@gmail.com>
2023-06-30 21:29:53 +0700

    Default gg key

--- client/src/configs/index.ts
+++ client/src/configs/index.ts
@@ -1,6 +1,6 @@
 const configs = {
   apiUrl: import.meta.env.VITE_APP_API_URL || 'https://shared-video-backend.onrender.com/api/v1',
-  googleAPIKey: import.meta.env.VITE_APP_GOOGLE_API_KEY
+  googleAPIKey: import.meta.env.VITE_APP_GOOGLE_API_KEY || 'AIzaSyC8HLe924zx0Z-SzI5HWbjQfqk7fmMs4Z8'
 }
 export const api = {
   token: '/authentication/token',
