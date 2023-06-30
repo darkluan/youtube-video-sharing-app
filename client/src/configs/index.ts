3e521a5150683e58edb4af28e2649dbb37263aa1
luanpb <luanphamyoyo@gmail.com>
2023-06-30 21:29:21 +0700

    Update default url api

--- client/src/configs/index.ts
+++ client/src/configs/index.ts
@@ -1,5 +1,5 @@
 const configs = {
-  apiUrl: import.meta.env.VITE_APP_API_URL || 'http://localhost:3001/api/v1',
+  apiUrl: import.meta.env.VITE_APP_API_URL || 'https://shared-video-backend.onrender.com/api/v1',
   googleAPIKey: import.meta.env.VITE_APP_GOOGLE_API_KEY
 }
 export const api = {
